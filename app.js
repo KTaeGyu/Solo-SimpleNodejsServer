77fa4725e02f339767712ed2fda48206a37291bd
KTG <xorb269@naver.com>
2023-10-30 00:38:52 +0900

    임시 서버 완료

--- app.js
+++ app.js
@@ -1,41 +1,86 @@
+// express
 const express = require("express");
 const app = express();
-const cors = require("cors");
 
+const cors = require("cors");
+const { json } = require("body-parser");
+
+// body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(cors());
 
+// DB
 let id = 2;
 const members = [
   {
     id: 1,
     email: "email@domain.com",
-    username: "username",
-    password: "password",
+    name: "name",
+    pwd: "pwd",
     nickname: "nickname",
   },
 ];
 
+// api server
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
-app.get("/api/member", (req, res) => {
+app.get("/auth/signup", (req, res) => {
   res.json(members);
 });
 
-app.post("/api/member", (req, res) => {
-  console.log(req.body);
-  const { email, username, password, nickname } = req.body;
+app.post("/auth/signup", (req, res) => {
+  const { email, name, pwd, nickname } = req.body;
+  if (email === "" || name === "" || pwd === "" || nickname === "") {
+    return res.json({ errorCode: 400 });
+  }
   members.push({
     id: id++,
     email,
-    username,
-    password,
+    name,
+    pwd,
     nickname,
   });
-  return res.send("success");
+  return res.json({ errorCode: 201 });
+});
+
+app.post("/auth/signup/email", (req, res) => {
+  const { email } = req.body;
+  for (const member in members) {
+    if (members[member].email === email) {
+      return res.json({ errorCode: 400 });
+    }
+  }
+  return res.json({ errorCode: 200 });
+});
+
+app.post("/auth/signup/nickname", (req, res) => {
+  const { nickname } = req.body;
+  for (const member in members) {
+    if (members[member].nickname === nickname) {
+      return res.json({ errorCode: 400 });
+    }
+  }
+  return res.json({ errorCode: 200 });
+});
+
+app.post("/auth/login", (req, res) => {
+  const { email, pwd } = req.body;
+  for (const member in members) {
+    if (members[member].email === email && members[member].pwd === pwd) {
+      return res.json({
+        errorCode: 200,
+        email: members[member].email,
+        name: members[member].name,
+        pwd: members[member].pwd,
+        nickname: members[member].nickname,
+      });
+    }
+  }
+  return res.json({ errorCode: 400 });
 });
 
 app.listen(4000, () => {
